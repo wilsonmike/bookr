d335d3901386664e9f01c5a71c8e19d4ffe07c0f
mike <michaelwilsonsj@hotmail.com>
2021-07-17 17:14:46 -0400

    migrated sqlite database and added models for publisher

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Publisher(models.Model):
+    """A company that publishes books"""
+    name = models.CharField(max_length=50,
+                            help_text="The name of the publisher.")
+    website = models.URLField(help_text="The publisher's website.")
+    email = models.EmailField(help_text="The publisher's email address.")
