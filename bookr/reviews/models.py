d227991c056dc47fcc94d9fb238f9ee2fa27892c
mike <michaelwilsonsj@hotmail.com>
2021-07-17 19:31:50 -0400

    added all models for reviews

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -1,3 +1,4 @@
+from django.contrib import auth
 from django.db import models
 
 
@@ -57,3 +58,15 @@ class BookContributor(models.Model):
 
 class Review(models.Model):
     content = models.TextField(help_text="The review text.")
+    rating = models.IntegerField(
+        help_text="The rating the reviewer has given.")
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+        help_text="The date and time the review was created.")
+    date_edited = models.DateTimeField(
+        null=True, help_text="The date and time the review was last edited.")
+    creator = models.ForeignKey(auth.get_user_model(),
+                                on_delete=models.CASCADE)
+    book = models.ForeignKey(Book,
+                             on_delete=models.CASCADE,
+                             help_text="The book that this review is for.")
